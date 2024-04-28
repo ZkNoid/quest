451422320f9502fdc3d82a68e99a0fd51a4d280e
asimaranov <a.simaranov@gmail.com>
2024-04-28 22:06:48 +0300

    Fixed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,50 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Roboto } from 'next/font/google'
-import localFont from 'next/font/local';
+import { Roboto } from "next/font/google";
+import localFont from "next/font/local";
 
 const roboto = Roboto({
-  variable: '--roboto',
-  weight: '300',
-  subsets: ['latin'],
-})
+  variable: "--roboto",
+  weight: "300",
+  subsets: ["latin"],
+});
 
 const arame = localFont({
-  variable: '--arame',
-  display: 'swap',
+  variable: "--arame",
+  display: "swap",
   src: [
     {
-      path: './fonts/Arame/Arame-Thin.ttf',
-      weight: '100',
-      style: 'thin',
+      path: "./fonts/Arame/Arame-Thin.ttf",
+      weight: "100",
+      style: "thin",
     },
     {
-      path: './fonts/Arame/Arame.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/Arame/Arame.ttf",
+      weight: "400",
+      style: "normal",
     },
   ],
 });
 export const metadata: Metadata = {
   title: "ZkNoid Quest",
   description: "Start your journey to the userfriendly ZK world",
-  openGraph: {
-    title: 'ZkNoid Quest',
-    description:
-      'Start your journey to the userfriendly ZK world',
-    url: 'https://quest.zknoid.io/',
-    images: '/meta-preview.png',
-    siteName: 'ZkNoid Quest',
-    type: 'website',
+  metadataBase: new URL("https://quest.zknoid.io/"),
+  alternates: {
+    canonical: "/",
   },
-
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  openGraph: {
+    title: "ZkNoid Quest",
+    description: "Start your journey to the userfriendly ZK world",
+    url: "https://quest.zknoid.io/",
+    images: "/meta-preview.png",
+    siteName: "ZkNoid Quest",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -48,7 +54,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${roboto.variable} ${arame.variable} overflow-x-hidden bg-dark`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${arame.variable} overflow-x-hidden bg-dark`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
