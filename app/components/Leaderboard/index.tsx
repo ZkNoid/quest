7eefac767609e024c16096af5759d1dfe2f9e27e
Neo-Gar <nvatlecov@mail.ru>
2024-10-07 22:57:20 +0700

    Add smooth scrolling

--- app/components/Leaderboard/index.tsx
+++ app/components/Leaderboard/index.tsx
@@ -42,6 +42,10 @@ export default function Leaderboard() {
     }
   }, [getLeaderboardQuery.data]);
 
+  const filteredLeaderboard = leaderboard
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 11);
+
   return (
     <section className={"flex flex-col gap-[1vw] w-full px-[6.4vw] pt-[10vw]"}>
       <Link
@@ -80,27 +84,41 @@ export default function Leaderboard() {
         >
           LEADERBOARD
         </span>
-        <div
-          className={
-            "bg-green hidden lg:!flex rounded-[0.26vw] px-[0.781vw] py-[0.417vw] flex-row justify-center items-center gap-[0.521vw]"
-          }
-        >
-          <span
-            className={"text-[1.042vw] font-roboto font-semibold text-black"}
-          >
-            Show my rank
-          </span>
-          <svg
-            width="24"
-            height="15"
-            viewBox="0 0 24 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className={"w-[1.25vw] h-[0.781vw]"}
-          >
-            <path d="M22 2L12 12L2 2" stroke="#141414" stroke-width="3" />
-          </svg>
-        </div>
+        {networkStore.address &&
+          filteredLeaderboard.find(
+            (item) => item.userAddress === networkStore.address,
+          ) && (
+            <button
+              className={
+                "hover:opacity-80 cursor-pointer bg-green hidden lg:!flex rounded-[0.26vw] px-[0.781vw] py-[0.417vw] flex-row justify-center items-center gap-[0.521vw]"
+              }
+              onClick={() => {
+                window.scrollTo({
+                  // @ts-ignore
+                  top: document.getElementById("userScore").offsetTop,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              <span
+                className={
+                  "text-[1.042vw] font-roboto font-semibold text-black"
+                }
+              >
+                Show my rank
+              </span>
+              <svg
+                width="24"
+                height="15"
+                viewBox="0 0 24 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className={"w-[1.25vw] h-[0.781vw]"}
+              >
+                <path d="M22 2L12 12L2 2" stroke="#141414" stroke-width="3" />
+              </svg>
+            </button>
+          )}
       </div>
       <div className={"grid grid-cols-4 lg:!grid-cols-5 py-[0.521vw]"}>
         <span
@@ -126,87 +144,89 @@ export default function Leaderboard() {
         </span>
       </div>
       <div className={"flex flex-col"}>
-        {leaderboard
-          .sort((a, b) => b.score - a.score)
-          .slice(0, 11)
-          .map((item, index) => (
-            <div
-              key={index}
-              className={
-                "grid grid-cols-4 lg:!grid-cols-5 py-[1.563vw] border-t last:border-b border-white"
-              }
+        {filteredLeaderboard.map((item, index) => (
+          <div
+            id={
+              networkStore.address && item.userAddress === networkStore.address
+                ? "userScore"
+                : undefined
+            }
+            key={index}
+            className={
+              "grid grid-cols-4 lg:!grid-cols-5 py-[1.563vw] border-t last:border-b border-white"
+            }
+          >
+            <span
+              className={cn(
+                "text-[3.765vw] lg:!text-[1.25vw] font-roboto font-medium text-white",
+                {
+                  "text-green":
+                    isMobile &&
+                    networkStore.address &&
+                    networkStore.address === item.userAddress,
+                },
+              )}
             >
-              <span
-                className={cn(
-                  "text-[3.765vw] lg:!text-[1.25vw] font-roboto font-medium text-white",
-                  {
-                    "text-green":
-                      isMobile &&
-                      networkStore.address &&
-                      networkStore.address === item.userAddress,
-                  },
-                )}
-              >
-                {index + 1}
-              </span>
-              <span
-                className={cn(
-                  "col-span-2 text-[3.765vw] lg:!text-[1.25vw] font-roboto font-medium text-white",
-                  {
-                    "text-green":
-                      isMobile &&
-                      networkStore.address &&
-                      networkStore.address === item.userAddress,
-                  },
-                )}
-              >
-                {formatAddress(item.userAddress, isMobile ? 6 : 15)}
-              </span>
-              <span
-                className={cn(
-                  "text-[3.765vw] lg:!text-[1.25vw] font-roboto font-medium text-white",
-                  {
-                    "text-green":
-                      isMobile &&
-                      networkStore.address &&
-                      networkStore.address === item.userAddress,
-                  },
-                )}
-              >
-                {item.score}
-              </span>
-              {!isMobile &&
-                networkStore.address &&
-                networkStore.address === item.userAddress && (
-                  <div
+              {index + 1}
+            </span>
+            <span
+              className={cn(
+                "col-span-2 text-[3.765vw] lg:!text-[1.25vw] font-roboto font-medium text-white",
+                {
+                  "text-green":
+                    isMobile &&
+                    networkStore.address &&
+                    networkStore.address === item.userAddress,
+                },
+              )}
+            >
+              {formatAddress(item.userAddress, isMobile ? 6 : 15)}
+            </span>
+            <span
+              className={cn(
+                "text-[3.765vw] lg:!text-[1.25vw] font-roboto font-medium text-white",
+                {
+                  "text-green":
+                    isMobile &&
+                    networkStore.address &&
+                    networkStore.address === item.userAddress,
+                },
+              )}
+            >
+              {item.score}
+            </span>
+            {!isMobile &&
+              networkStore.address &&
+              networkStore.address === item.userAddress && (
+                <div
+                  className={
+                    "flex flex-row items-center justify-center gap-[0.781vw]"
+                  }
+                >
+                  <svg
+                    width="30"
+                    height="28"
+                    viewBox="0 0 30 28"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={"w-[1.563vw] h-[1.458vw]"}
+                  >
+                    <path
+                      d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
+                      fill="#D4FF33"
+                    />
+                  </svg>
+                  <span
                     className={
-                      "flex flex-row items-center justify-center gap-[0.781vw]"
+                      "text-[3.765vw] lg:!text-[1.25vw] font-roboto text-green"
                     }
                   >
-                    <svg
-                      width="30"
-                      height="28"
-                      viewBox="0 0 30 28"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className={"w-[1.563vw] h-[1.458vw]"}
-                    >
-                      <path
-                        d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
-                        fill="#D4FF33"
-                      />
-                    </svg>
-                    <span
-                      className={
-                        "text-[3.765vw] lg:!text-[1.25vw] font-roboto text-green"
-                      }
-                    >
-                      Your place
-                    </span>
-                  </div>
-                )}
-            </div>
-          ))}
+                    Your place
+                  </span>
+                </div>
+              )}
+          </div>
+        ))}
       </div>
     </section>
   );
