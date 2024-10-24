e1e1ffd8a95f95957e51c86bfbb04c3d5715e9b1
asimaranov <a.simaranov@gmail.com>
2024-10-24 15:00:29 +0300

    Updated text

--- components/Section1/index.tsx
+++ components/Section1/index.tsx
@@ -21,7 +21,7 @@ const ClockBlock = ({ number, label }: { number: number; label: string }) => {
 const TimerLabel = () => {
   return (
     <div className="bg-dark w-[75.294vw] lg:w-[50vw] h-[7.059vw] lg:!h-[3.26vw] rounded-[0.6vw] text-green font-arame flex items-center justify-center text-[4.235vw] lg:!text-[2.344vw]">
-      TESTNET ENDS IN
+      TESTNET ENDS October 28th
     </div>
   );
 };
